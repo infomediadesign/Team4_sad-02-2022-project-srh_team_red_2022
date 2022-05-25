fc20131818403bb9326f5905445a481ee4e47a57
ARCH071995 <99989075+ARCH071995@users.noreply.github.com>
2022-05-25 21:22:45 +0200

    Testing

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
     return (
         <div>
-            <h1>Welcome to the Postagram Memory App</h1>
+            <h1>Welcome to the Postagram Memory Application</h1>
         </div>
     );
 }
