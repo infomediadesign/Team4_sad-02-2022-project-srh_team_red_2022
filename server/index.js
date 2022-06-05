0595326cdacca86612351327f538efd44572834d
ARCH071995 <99989075+ARCH071995@users.noreply.github.com>
2022-06-05 20:03:24 +0200

    Connection to MongoDB Atlas Cloud and Application done using mongoose(library)

--- server/index.js
+++ server/index.js
@@ -6,13 +6,28 @@ import cors from 'cors';
 const app = express();
 
 //Setup of the body parser so that we can send our requests for e.g: image and imageUrl
-app.use(bodyParser.json({ 
+app.use(bodyParser.json({
     limit: "40mb",
-    extennded: true
-}));
+    extended: true
+}))
 
 app.use(bodyParser.urlencoded({
     limit: "40mb",
-    extennded: true
-}));
+    extended: true
+}))
 
+// To initialise cors and call it as a function
+app.use(cors());
+
+// Setup/Connection for Mongodb cloud database using Atlas to create cluster
+// Connect Server Application to the datatbase https://www.mongodb.com/cloud/atlas
+
+const CONNECTION_URL = 'mongodb+srv://srhmernproject:Teamred12345@cluster0.y8eulcd.mongodb.net/?retryWrites=true&w=majority'; //In real Application the creditials should be secured so we will later change this and store this in the environment variables for security reasons. 
+const PORT = process.env.PORT||5000;
+
+mongoose.connect(CONNECTION_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }) //We have set to true to avoid the warnings and errors in the console.
+    .then(() => app.listen(PORT, () => console.log(`Server Running on port: http://localhost:${PORT}`))) //Using .then async function to return a promise i.e success message if server is running successfully
+    .catch((error) => console.log(`${error} did not connect`)); //to catch error if server is not running properly.
