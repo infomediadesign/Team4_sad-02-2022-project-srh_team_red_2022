d167af095da12ab86f97baa46511bbf301a001eb
ARCH071995 <99989075+ARCH071995@users.noreply.github.com>
2022-06-05 15:53:00 +0200

    Added the setup for body parser and cors and the size of the images to be posted as the user will be sending requests to upload images

--- server/index.js
+++ server/index.js
@@ -2,3 +2,17 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const app = express();
+
+//Setup of the body parser so that we can send our requests for e.g: image and imageUrl
+app.use(bodyParser.json({ 
+    limit: "40mb",
+    extennded: true
+}));
+
+app.use(bodyParser.urlencoded({
+    limit: "40mb",
+    extennded: true
+}));
+
