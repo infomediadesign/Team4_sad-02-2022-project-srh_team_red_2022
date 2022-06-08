39d78e72503af026d0a0c5699c0738da7c5cac15
“raysamsoorya” <raysamsoorya@gmail.com>
2022-06-08 19:41:45 +0530

    LIKE function stage 2 implemented

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -13,12 +13,19 @@ export default (posts = [], action) => {
     case 'CREATE':
       return [...posts, action.payload];
       // after the API call , action.payload is the newly updated post
+       // LIKE has same Logic as update function
     case 'UPDATE':
+    case 'LIKE':
+
       return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
     
     // We use filter because we keep all posts which are NOT equal to action.payload
-      case 'DELETE':
+    case 'DELETE':
       return posts.filter((post) => post._id !== action.payload);
+
+     
+    
+
     default:
       return posts;
   }
