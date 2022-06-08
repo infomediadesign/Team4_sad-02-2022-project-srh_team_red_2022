7d5ebe06da3029b2d0acf9f73f858a097a64d576
“raysamsoorya” <raysamsoorya@gmail.com>
2022-06-08 21:40:40 +0530

    Minor Like error solved

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -47,12 +47,12 @@ export const deletePost = async(req,res) => {
 }
 
 export const likePost = async(req, res) => {
-    const {id} = req.params
+    const {id} = req.params;
 
     if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post exists with that id');
 
     // this is the post being liked
-    const post = PostMessage.findById(id);
+    const post = await PostMessage.findById(id);
     
     const updatedPost = await PostMessage.findByIdAndUpdate(id,{ likeCount: post.likeCount + 1}, { new: true})
 
